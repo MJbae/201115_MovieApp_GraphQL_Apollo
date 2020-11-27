a0cd406c6198d5e0c8ab232334ca5f3804f4a831
MJbae <bmj5208@gmail.com>
2020-11-27 12:24:40 +0900

    chore: 환경설정 완료(tutorial)

--- 201126_PDIFS_GraphQL_Apollo/server/src/index.js
+++ 201126_PDIFS_GraphQL_Apollo/server/src/index.js
@@ -1,45 +1,16 @@
-const { ApolloServer, gql } = require("apollo-server");
-const MoviesAPI = require("./datasources/movie");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const LaunchAPI = require("./datasources/launch");
 
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-  }
-
-  type Query {
-    movies: [Movie]!
-  }
-`;
-
-const dataSources = () => ({
-  launchAPI: new LaunchAPI(),
-  MoviesAPI: new MoviesAPI(),
-});
-
-const resolvers = {
-  Query: {
-    movies: async (_, __, { dataSources }) => {
-      const allMovies = await dataSources.MoviesAPI.getMovies();
-
-      return allMovies;
-    },
-  },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
-
-module.exports = {
-  dataSources,
+const server = new ApolloServer({
   typeDefs,
   resolvers,
-  ApolloServer,
-  MoviesAPI,
-};
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+  }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
